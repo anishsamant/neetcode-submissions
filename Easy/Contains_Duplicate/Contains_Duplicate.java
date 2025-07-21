379194d725b1b1879377d6b589fc4fe5997b872d
ANISH SAMANT <anish.samant97@gmail.com>
2025-07-21 17:44:38 -0400

    Add Easy/Contains_Duplicate/Contains_Duplicate.java

--- Easy/Contains_Duplicate/Contains_Duplicate.java
+++ Easy/Contains_Duplicate/Contains_Duplicate.java
@@ -1,16 +1,13 @@
 class Solution {
-    /**
-     * @param {number[]} nums
-     * @return {boolean}
-     */
-    hasDuplicate(nums) {
-        let mySet = new Set();
-        for (let num of nums) {
-            mySet.add(num);
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> mySet = new HashSet<>();
+        for (int num : nums) {
+            if (!mySet.add(num)) {
+                return true;
+            } 
         }
 
-        return nums.length != mySet.size;
+        return false;
     }
 }
-
 
