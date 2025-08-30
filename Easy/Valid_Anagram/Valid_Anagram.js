b18e44342b6c5415b5f642479e9bfe2071e54f95
ANISH SAMANT <anish.samant97@gmail.com>
2025-08-30 19:45:33 -0400

    add/update: Valid Anagram (javascript)

--- Easy/Valid_Anagram/Valid_Anagram.js
+++ Easy/Valid_Anagram/Valid_Anagram.js
@@ -1,20 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
     isAnagram(s, t) {
         if (s == null && t == null) return true;
         if (s == null || t == null || s.length != t.length) return false;
 
-
         let myArray = new Array(26).fill(0);
         for (let i = 0; i < s.length; i++) {
             myArray[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
-        }
             myArray[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
-            if (count != 0) return false;
-     * @param {string} t
-     * @return {boolean}
-     */
-        for (let count of myArray) {
+        }
+
         console.log(myArray);
+        for (let count of myArray) {
+            if (count != 0) return false;
         }
 
         return true;
+    }
+}
+
 
