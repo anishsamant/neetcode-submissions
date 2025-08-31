d419ebdde8c7b5b0453886315774a7ee22388be8
ANISH SAMANT <anish.samant97@gmail.com>
2025-08-30 21:44:41 -0400

    add/update: Group Anagrams (javascript)

--- Medium/Group_Anagrams/Group_Anagrams.js
+++ Medium/Group_Anagrams/Group_Anagrams.js
@@ -1,26 +1,24 @@
+        let res = new Map();
+        for (let str of strs) {
+            let count = new Array(26).fill(0);
             for (let c of str) {
                 count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
             }
-            let count = new Array(26).fill(0);
             let key = count.join(',');
             if (!res.has(key)) {
                 res.set(key,[]);
             }
-            let value = res.get(key);
-            value.push(str);
-        for (let str of strs) {
-        let res = new Map();
+            res.get(key).push(str);
+        }
+
      */
     groupAnagrams(strs) {
      * @param {string[]} strs
      * @return {string[][]}
-class Solution {
-    /**
-            res.set(key,  value);
-        }
-
-        return Array.from(res.values());
+        return Array.from(res.values()); // return list of list of string
     }
 }
 
+    /**
+class Solution {
 
