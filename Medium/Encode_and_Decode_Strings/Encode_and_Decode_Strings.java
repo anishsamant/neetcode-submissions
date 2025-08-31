7b340a154670809422a5b7ac4206a2d4e7e5b2b5
ANISH SAMANT <anish.samant97@gmail.com>
2025-08-31 18:36:43 -0400

    add/update: Encode and Decode Strings (java)

---
+++ Medium/Encode_and_Decode_Strings/Encode_and_Decode_Strings.java
@@ -0,0 +1,29 @@
+    public String encode(List<String> strs) {
+    }
+
+    public List<String> decode(String str) {
+        return ans;
+    }
+        for (String s : strs) {
+            encoded += s.length() + "#" + s;
+        }
+        String encoded = "";
+
+        return encoded;
+        int i = 0;  
+            int j = i;
+        }
+            while (str.charAt(j) != '#') {
+        while (i < str.length()) {
+                j++;
+            }
+            int l = Integer.parseInt(str.substring(i, j));
+            j++;
+            String word = str.substring(j, endIndex);
+            int endIndex = j + l;
+            ans.add(word);
+        List<String> ans = new ArrayList<>();  
+            i = endIndex;
+}
+
+
