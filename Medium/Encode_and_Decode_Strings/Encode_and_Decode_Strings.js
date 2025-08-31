7a82c4f5c919f5a2ef2358ac2dd2e34046f1e199
ANISH SAMANT <anish.samant97@gmail.com>
2025-08-31 19:44:11 -0400

    add/update: Encode and Decode Strings (javascript)

--- Medium/Encode_and_Decode_Strings/Encode_and_Decode_Strings.js
+++ Medium/Encode_and_Decode_Strings/Encode_and_Decode_Strings.js
@@ -1,10 +1,31 @@
-class Solution {
-    /**
-     * @param {string[]} strs
-     * @returns {string}
-     */
-    encode(strs) {
-        let encoded = '';
-        for (let str of strs) {
-            encoded += str.length + "#" + str;
-        }
+class Solution {
+    encode(strs) {
+        let encoded = '';
+        for (let str of strs) {
+            encoded += str.length + "#" + str;
+        }
+        
+        return encoded;
+    }
+
+    decode(str) {
+        let strs = [];
+        let i = 0;
+        while (i < str.length) {
+            let j = i;
+            while (str[j] !== "#") {
+                j++;
+            }
+            let l = parseInt(str.slice(i, j));
+            j++;
+            let endIndex = j + l;
+            let word = str.slice(j, endIndex);
+            strs.push(word);
+            i = endIndex;
+        }
+
+        return strs;
+    }
+}
+
+
