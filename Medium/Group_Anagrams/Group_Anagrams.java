2d1a4df80d41539f5a9ad7894046abba290b861b
ANISH SAMANT <anish.samant97@gmail.com>
2025-08-30 22:02:37 -0400

    add/update: Group Anagrams (java)

--- Medium/Group_Anagrams/Group_Anagrams.java
+++ Medium/Group_Anagrams/Group_Anagrams.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> myMap = new HashMap<>();
         for (String s : strs) {
-    }
+            int[] count = new int[26];
             for (char c : s.toCharArray()) {
-        }
                 count[c - 'a']++;
             }
-            int[] count = new int[26];
             String key = Arrays.toString(count);
             if (!myMap.containsKey(key)) {
-        Map<String, List<String>> myMap = new HashMap<>();
                 myMap.put(key, new ArrayList<>());
             }
-}
             myMap.get(key).add(s);
+        }
 
         return new ArrayList<>(myMap.values());
+    }
+}
 
 
