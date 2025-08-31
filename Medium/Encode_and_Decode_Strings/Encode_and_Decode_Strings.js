6e591059449135cfef557873132e6fe7f516bba0
ANISH SAMANT <anish.samant97@gmail.com>
2025-08-31 17:13:10 -0400

    add/update: Encode and Decode Strings (javascript)

---
+++ Medium/Encode_and_Decode_Strings/Encode_and_Decode_Strings.js
@@ -0,0 +1,24 @@
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+        for (let str of strs) {
+    }
+            encoded += str + "_";
+        }
+        let encoded = "";
+        
+     */
+    decode(str) {
+    }
+    encode(strs) {
+     */
+     * @returns {string}
+     * @param {string[]} strs
+    /**
+class Solution {
+        return str.split("_");
+        return encoded.slice(0, -1);
+}
+
+
