7afb2077a7b7090e902a96801aefd58aa67d2e68
ANISH SAMANT <anish.samant97@gmail.com>
2025-08-31 16:16:08 -0400

    add/update: Top K Frequent Elements (java)

--- Medium/Top_K_Frequent_Elements/Top_K_Frequent_Elements.java
+++ Medium/Top_K_Frequent_Elements/Top_K_Frequent_Elements.java
@@ -1,3 +1,14 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> myMap = new HashMap<>();
+        List<List<Integer>> freq = new ArrayList<>(nums.length + 1);
+
+        for (int i = 0; i < nums.length + 1; i++) {
+            freq.add(new ArrayList<>());
+        }
+
+        for (int num : nums) {
+            myMap.put(num, myMap.getOrDefault(num, 0) + 1);
         }
 
         for (Map.Entry<Integer, Integer> entry : myMap.entrySet()) {
@@ -5,18 +16,4 @@
         }
 
         int[] res = new int[k];
-        int count = 0;
-        for (int i = freq.size() - 1; i > 0 && count < k; i--) {
-            for (int val : freq.get(i)) {
-        for (int num : nums) {
-            myMap.put(num, myMap.getOrDefault(num, 0) + 1);
-        }
-
-            freq.add(new ArrayList<>());
-        List<List<Integer>> freq = new ArrayList<>(nums.length + 1);
-
-class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> myMap = new HashMap<>();
-        for (int i = 0; i < nums.length + 1; i++) {
 
