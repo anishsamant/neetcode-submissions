9ae12d31a211bcffd1db5e75b2273bc99eda670f
ANISH SAMANT <anish.samant97@gmail.com>
2025-08-31 16:40:58 -0400

    add/update: Two Sum (javascript)

--- Easy/Two_Sum/Two_Sum.js
+++ Easy/Two_Sum/Two_Sum.js
@@ -1,3 +1,5 @@
+class Solution {
+    /**
      * @param {number[]} nums
      * @param {number} target
      * @return {number[]}
@@ -6,11 +8,10 @@
         let myMap = new Map();
         for (let i = 0; i < nums.length; i++) {
             if (myMap.has(target - nums[i])) {
-        }
-        let ans = new Array(2);
                 return [myMap.get(target - nums[i]), i];
             }
             myMap.set(nums[i], i);
+        }
 
         return [];
     }
