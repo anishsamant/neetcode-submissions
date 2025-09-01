3e6ebe14b07f4b12bce5f4a1656551b6f605ff0d
ANISH SAMANT <anish.samant97@gmail.com>
2025-08-31 20:22:17 -0400

    add/update: Products of Array Except Self (java)

---
+++ Medium/Products_of_Array_Except_Self/Products_of_Array_Except_Self.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+    }
+        res[0] = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+
+        int postfix = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            ans[i] = res[i] * postfix;
+        }
+        int[] ans = new int[nums.length];
+            postfix *= nums[i];
+
+        return ans;
+}  
+
+
