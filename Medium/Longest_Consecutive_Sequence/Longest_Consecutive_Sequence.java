ecc46d952c0d8da6a4273389a0fc15f9ed3d2aac
ANISH SAMANT <anish.samant97@gmail.com>
2025-09-01 01:34:12 -0400

    add/update: Longest Consecutive Sequence (java)

--- Medium/Longest_Consecutive_Sequence/Longest_Consecutive_Sequence.java
+++ Medium/Longest_Consecutive_Sequence/Longest_Consecutive_Sequence.java
@@ -1,16 +1,20 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> mySet = new HashSet<>();
+        for (int num : nums) {
             mySet.add(num);
         }
 
+        int res = 0;
         for (int num : mySet) {
             if (!mySet.contains(num - 1)) {
-        }
                 int l = 1;
-            }
                 while (mySet.contains(num + l)) {
                     l++;
                 }
                 res = Math.max(res, l);
-        int res = 0;
+            }
+        }
 
         return res;
     }
