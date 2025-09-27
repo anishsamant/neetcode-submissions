5a9e67e604e65f5df50c11b8328f26b3306648f6
ANISH SAMANT <anish.samant97@gmail.com>
2025-09-26 21:32:25 -0400

    add/update: Best Time to Buy and Sell Stock (java)

--- Easy/Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock.java
+++ Easy/Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock.java
@@ -1,19 +1,19 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        while (j < prices.length) {
-    }
-            if (prices[i] < prices[j]) {
-        }
         int maxProfit = 0;
         int i = 0, j = 1;
+        while (j < prices.length) {
+            if (prices[i] < prices[j]) {
                 int profit = prices[j] - prices[i];
-            } else {
                 maxProfit = Math.max(maxProfit, profit);
+            } else {
                 i = j;
             }
-}
             j++;
+        }
 
         return maxProfit;
+    }
+}
 
 
