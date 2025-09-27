d225a0fc70ab07c90ca4dcf6774ade984bbde5ac
ANISH SAMANT <anish.samant97@gmail.com>
2025-09-26 21:16:53 -0400

    add/update: Two Sum (java)

--- Easy/Two_Sum/Two_Sum.java
+++ Easy/Two_Sum/Two_Sum.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> myMap = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (myMap.containsKey(target - nums[i])) {
-                return new int[]{myMap.get(target - nums[i]), i};
-            }
-            myMap.put(nums[i], i);
-        }
-
-        // default return
-        return new int[2];
+       Map<Integer, Integer> myMap = new HashMap<>();
     }
+       for (int i = 0; i < nums.length; i++) {
+            if (myMap.containsKey(num)) {
+       }
+            int num = target - nums[i];
+                return new int[]{myMap.get(num), i};
+            }
 }
+            myMap.put(nums[i], i);
+
+       return new int[2];
 
 
