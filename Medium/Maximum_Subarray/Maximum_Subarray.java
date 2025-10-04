e0de739a91849369ae491b8cd8a9ce69e70d5f52
ANISH SAMANT <anish.samant97@gmail.com>
2025-10-04 15:48:07 -0400

    add/update: Maximum Subarray (java)

---
+++ Medium/Maximum_Subarray/Maximum_Subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSubSum = nums[0];
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+            if (sum < num) {
+                maxSubSum = Math.max(maxSubSum, num);
+                sum = num;
+            }
+        }
+
+        return Math.max(maxSubSum, sum);
+    }
+}
+
+
