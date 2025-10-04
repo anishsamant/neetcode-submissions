7f91e947b17b838a68414988ab4731bb146cae70
ANISH SAMANT <anish.samant97@gmail.com>
2025-10-04 18:37:09 -0400

    add/update: Find Minimum in Rotated Sorted Array (java)

--- Medium/Find_Minimum_in_Rotated_Sorted_Array/Find_Minimum_in_Rotated_Sorted_Array.java
+++ Medium/Find_Minimum_in_Rotated_Sorted_Array/Find_Minimum_in_Rotated_Sorted_Array.java
@@ -3,10 +3,10 @@ class Solution {
         int l = 0, r = nums.length - 1;
         while (l < r) {
             int m = l + (r - l) / 2;
-            if (nums[m] >= nums[l]) {
-                l = m + 1;
-            } else {
+            if (nums[m] < nums[r]) {
                 r = m;
+            } else {
+                l = m + 1;
             }
         }
 
