387ec5f0484e511a4c2c092557c7aff88313ac59
ANISH SAMANT <anish.samant97@gmail.com>
2025-10-09 12:29:22 -0400

    add/update: Merge K Sorted Linked Lists (javascript)

---
+++ Hard/Merge_K_Sorted_Linked_Lists/Merge_K_Sorted_Linked_Lists.js
@@ -0,0 +1,34 @@
+    mergeKLists(lists) {
+        if (lists.length == 0) return null;
+        for (let i = 1; i < lists.length; i++) {
+            lists[i] = this.mergeTwoSorted(lists[i], lists[i - 1]);
+        }
+        return lists[lists.length - 1];
+    }
+    mergeTwoSorted(list1, list2) {
+        let node = new ListNode(0);
+        let curr = node;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+class Solution {
+ *     }
+ * } */
+ *         this.val = val;
+ *         this.next = next;
+        }
+        if (list1 != null) curr.next = list1;
+        else curr.next = list2;
+        return node.next;
+    }
+ *     constructor(val = 0, next = null) {
+ /** class ListNode {
+}
+
+
